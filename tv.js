62edeab793559031c71a15a0880de226e0e62614
belangerruiz-source <belangerruiz@gmail.com>
2026-03-17 16:59:45 -0600

    Add files via upload

--- tv.js
+++ tv.js
@@ -1,6 +1,8 @@
 let canales = [];
 let canalActual = 0;
-let inicioCanal = 0;
+
+// TIEMPO GLOBAL (clave 🔥)
+const inicioGlobal = Math.floor(Date.now() / 1000);
 
 fetch("canales.json")
   .then(res => res.json())
@@ -15,26 +17,33 @@ function renderCanales() {
   cont.innerHTML = "";
 
   canales.forEach((c, i) => {
-    let div = document.createElement("div");
-    div.innerText = c.nombre;
-    div.onclick = () => cargarCanal(i);
-    cont.appendChild(div);
+    let btn = document.createElement("div");
+    btn.innerText = "📺 " + c.nombre;
+    btn.className = "canal";
+    btn.onclick = () => cambiarCanal(i);
+    cont.appendChild(btn);
   });
+}
+
+function cambiarCanal(i) {
+  canalActual = i;
+  reproducir();
+  generarEPG();
 }
 
 function cargarCanal(i) {
   canalActual = i;
-  inicioCanal = Date.now() / 1000;
-
   reproducir();
   generarEPG();
 }
 
 function reproducir() {
   const canal = canales[canalActual];
-  const ahora = Date.now() / 1000;
 
-  let tiempo = ahora - inicioCanal;
+  const totalDuracion = canal.duraciones.reduce((a,b)=>a+b,0);
+
+  let tiempo = inicioGlobal % totalDuracion;
+
   let acumulado = 0;
 
   for (let i = 0; i < canal.videos.length; i++) {
@@ -48,14 +57,12 @@ function reproducir() {
 
     acumulado += dur;
   }
-
-  inicioCanal = ahora;
-  reproducir();
 }
 
 function playVideo(id, start) {
   const iframe = document.getElementById("video");
-  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&start=${Math.floor(start)}`;
+
+  iframe.src = `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&start=${Math.floor(start)}`;
 }
 
 function generarEPG() {
@@ -64,9 +71,20 @@ function generarEPG() {
 
   cont.innerHTML = "";
 
-  canal.videos.forEach((v, i) => {
+  let tiempo = new Date();
+
+  canal.videos.slice(0,5).forEach((v, i) => {
+
+    let dur = canal.duraciones[i];
+
+    let inicio = new Date(tiempo);
+    let fin = new Date(tiempo.getTime() + dur*1000);
+
     let div = document.createElement("div");
-    div.innerText = `Video ${i+1} (${canal.duraciones[i]}s)`;
+    div.innerText = `${inicio.toLocaleTimeString()} - ${fin.toLocaleTimeString()}`;
+
     cont.appendChild(div);
+
+    tiempo = fin;
   });
 }
