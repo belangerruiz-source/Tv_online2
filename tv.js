4232a06ac5f309e14b07fd82fcdcc0c16bbbcdbf
belangerruiz-source <belangerruiz@gmail.com>
2026-03-22 15:04:08 -0600

    Add files via upload

--- tv.js
+++ tv.js
@@ -1,63 +1,55 @@
-document.body.style.background = "black";
-
-// ===============================
-const cont = document.getElementById("canales");
-
-if (!cont) {
-  document.body.innerHTML = "<h1 style='color:red'>NO EXISTE DIV #canales</h1>";
-} else {
-  cont.innerHTML = "<h2 style='color:lime'>JS FUNCIONANDO</h2>";
-}
+let canalActual = 0;
+let canales = [];
 
 // ===============================
 fetch("canales.json")
   .then(res => res.json())
   .then(data => {
-    cont.innerHTML += "<p style='color:white'>JSON cargado OK</p>";
-
-    data.forEach((c, i) => {
-      let div = document.createElement("div");
-      div.style.color = "yellow";
-      div.style.padding = "10px";
-      div.innerText = (i+1) + ". " + c.nombre;
-      cont.appendChild(div);
-    });
-
-    iniciarYouTube(data);
-  })
-  .catch(err => {
-    cont.innerHTML += "<p style='color:red'>ERROR JSON</p>";
-    console.error(err);
+    canales = data;
+    crearCanales();
+    reproducir();
   });
 
 // ===============================
-function iniciarYouTube(canales) {
+function reproducir() {
+  const canal = canales[canalActual];
 
-  const playerDiv = document.getElementById("player");
+  if (!canal || !canal.programas.length) return;
 
-  if (!playerDiv) {
-    document.body.innerHTML += "<h1 style='color:red'>NO EXISTE #player</h1>";
-    return;
-  }
+  const video = canal.programas[0].id;
 
-  playerDiv.innerHTML = "<p style='color:white'>Cargando YouTube...</p>";
+  const player = document.getElementById("player");
 
-  const tag = document.createElement("script");
-  tag.src = "https://www.youtube.com/iframe_api";
-  document.body.appendChild(tag);
+  player.innerHTML = `
+    <iframe 
+      width="100%" 
+      height="100%" 
+      src="https://www.youtube.com/embed/${video}?autoplay=1&mute=0"
+      frameborder="0"
+      allow="autoplay; encrypted-media"
+      allowfullscreen>
+    </iframe>
+  `;
+}
 
-  window.onYouTubeIframeAPIReady = function () {
+// ===============================
+function crearCanales() {
+  const cont = document.getElementById("canales");
+  cont.innerHTML = "";
 
-    playerDiv.innerHTML = "";
+  canales.forEach((c, i) => {
+    const div = document.createElement("div");
 
-    const player = new YT.Player("player", {
-      width: "100%",
-      height: "300",
-      videoId: canales[0].programas[0].id,
-      playerVars: {
-        autoplay: 1,
-        mute: 1
-      }
-    });
-  };
+    div.innerText = (i + 1) + ". " + c.nombre;
+    div.style.padding = "12px";
+    div.style.cursor = "pointer";
+    div.style.color = "yellow";
+
+    div.onclick = () => {
+      canalActual = i;
+      reproducir();
+    };
+
+    cont.appendChild(div);
+  });
 }
